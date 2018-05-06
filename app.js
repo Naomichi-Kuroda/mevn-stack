494bce13d6f8755011b617446a9c57c747a435ea
Naomichi Kuroda <naomichi.kuroda.jp@gmail.com>
2018-05-06 12:28:08 +0900

    Update cors setting

--- app.js
+++ app.js
@@ -9,6 +9,20 @@ let mongoose = require('mongoose');
 let character = require('./routes/character');
 let app = express();
 
+// allow cors
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', req.headers.origin);
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Max-Age', '86400');
+  next();
+});
+
+app.options('*', function (req, res) {
+  res.sendStatus(200);
+});
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({'extended':'false'}));
